7d02b1a5cff2ec50b970f1be43409e3582483e61
duende14 <84480089+duende14@users.noreply.github.com>
2023-11-26 13:34:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//Function nueva pestaña
+
+function myFunction() {
+  window.location.href="https://programminghead.com/submit-button-redirect-to-another-page-in-html/";
+  }
+
+  function Home() {
+    window.location.href="./index.html"  
+  }
+
+function Password(){
+  window.location.href="./password.html"
+}
